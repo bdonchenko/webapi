23b2036804a8c95597978f072749da50f73005d8
Bohdan Donchenko <bohdan@ves.io>
2020-03-04 15:38:49 +0200

    added xunit

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace webapi
+namespace WebApi
 {
     public class Startup
     {
@@ -30,6 +30,8 @@ namespace webapi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
